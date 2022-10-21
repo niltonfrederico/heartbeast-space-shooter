e3ea4d2b5c24e94afbabe4bee4ef8b2fd1880715
Nilton Frederico Teixeira <9078708+niltonfrederico@users.noreply.github.com>
2022-10-20 21:39:22 -0300

    feat: added start screen actions

--- code/StartMenu.gd
+++ code/StartMenu.gd
@@ -4,9 +4,11 @@ extends Node
 func change_to_main_scene():
 	get_tree().change_scene("res://Main.tscn")
 	
-func _input(event):
-	if event is InputEventKey and event.is_pressed():
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
 		change_to_main_scene()
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
 
 func _on_Button_pressed():
 	change_to_main_scene()
